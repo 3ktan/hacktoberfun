e088f0781cd67dfac5c890d38abf1bfe0ccee0eb
3ktan <3ktan.95@gmail.com>
2018-10-17 00:39:56 +0700

    division

---
+++ bgail/division.py
@@ -0,0 +1,10 @@
+def division(x,y):
+	return x/y;
+def main():
+	print("division")
+    x = int(input('Enter first num: '))
+    y = int(input('Enter second num: '))
+    print(sum(x,y))
+
+if __name__ == '__main__':
+    main()
